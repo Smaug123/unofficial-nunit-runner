9ff11da8afb4d6e540ab7ca3a3f5fb70b586a1d0
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-04 23:51:57 +0100

    Oops, make it actually fail

--- TestRunner/Program.fs
+++ TestRunner/Program.fs
@@ -487,18 +487,23 @@ module Program =
 
         let assy = Assembly.LoadFrom testDll.FullName
 
-        assy.ExportedTypes
-        // TODO: NUnit nowadays doesn't care if you're a TestFixture or not
-        |> Seq.filter (fun ty ->
-            ty.CustomAttributes
-            |> Seq.exists (fun attr -> attr.AttributeType.FullName = "NUnit.Framework.TestFixtureAttribute")
-        )
-        |> Seq.iter (fun ty ->
-            let testFixture = TestFixture.parse ty
+        let anyFailures =
+            assy.ExportedTypes
+            // TODO: NUnit nowadays doesn't care if you're a TestFixture or not
+            |> Seq.filter (fun ty ->
+                ty.CustomAttributes
+                |> Seq.exists (fun attr -> attr.AttributeType.FullName = "NUnit.Framework.TestFixtureAttribute")
+            )
+            |> Seq.fold
+                (fun anyFailures ty ->
+                    let testFixture = TestFixture.parse ty
 
-            match TestFixture.run filter testFixture with
-            | 0 -> ()
-            | i -> eprintfn $"%i{i} tests failed"
-        )
+                    match TestFixture.run filter testFixture with
+                    | 0 -> anyFailures
+                    | i ->
+                        eprintfn $"%i{i} tests failed"
+                        true
+                )
+                false
 
-        0
+        if anyFailures then 1 else 0
