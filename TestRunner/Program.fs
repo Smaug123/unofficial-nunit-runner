c8975e61ff372dea4e0aa0214c00c42e4dd0ad96
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-05 00:07:10 +0100

    Finish fix

--- TestRunner/Program.fs
+++ TestRunner/Program.fs
@@ -472,7 +472,6 @@ module TestFixture =
         )
 
 module Program =
-    [<EntryPoint>]
     let main argv =
         let testDll, filter =
             match argv |> List.ofSeq with
@@ -485,25 +484,42 @@ module Program =
             | Some filter -> TestFixture.shouldRun filter
             | None -> fun _ _ -> true
 
+        // Fix for https://github.com/Smaug123/unofficial-nunit-runner/issues/8
+        // Set AppContext.BaseDirectory to where the test DLL is.
+        // (This tells the DLL loader to look next to the test DLL for dependencies.)
+        let oldBaseDir = AppContext.BaseDirectory
+        AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", testDll.Directory.FullName)
         let assy = Assembly.LoadFrom testDll.FullName
 
         let anyFailures =
-            assy.ExportedTypes
-            // TODO: NUnit nowadays doesn't care if you're a TestFixture or not
-            |> Seq.filter (fun ty ->
-                ty.CustomAttributes
-                |> Seq.exists (fun attr -> attr.AttributeType.FullName = "NUnit.Framework.TestFixtureAttribute")
-            )
-            |> Seq.fold
-                (fun anyFailures ty ->
-                    let testFixture = TestFixture.parse ty
-
-                    match TestFixture.run filter testFixture with
-                    | 0 -> anyFailures
-                    | i ->
-                        eprintfn $"%i{i} tests failed"
-                        true
+            try
+                assy.ExportedTypes
+                // TODO: NUnit nowadays doesn't care if you're a TestFixture or not
+                |> Seq.filter (fun ty ->
+                    ty.CustomAttributes
+                    |> Seq.exists (fun attr -> attr.AttributeType.FullName = "NUnit.Framework.TestFixtureAttribute")
                 )
-                false
+                |> Seq.fold
+                    (fun anyFailures ty ->
+                        let testFixture = TestFixture.parse ty
+
+                        match TestFixture.run filter testFixture with
+                        | 0 -> anyFailures
+                        | i ->
+                            eprintfn $"%i{i} tests failed"
+                            true
+                    )
+                    false
+            finally
+                AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", oldBaseDir)
 
         if anyFailures then 1 else 0
+
+    [<EntryPoint>]
+    let reallyMain argv =
+        // Hack to make sure `finally`s get run.
+        // (The runtime doesn't define which `finally`s, if any, run when an uncaught exception terminates execution.)
+        try
+            main argv
+        with _ ->
+            reraise ()
