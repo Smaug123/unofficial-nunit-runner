66a15bbc44a55a3a85a4bdaa6b471722da5329e8
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-05 13:54:07 +0100

    And fix

--- Consumer/TestNonStatic.fs
+++ Consumer/TestNonStatic.fs
@@ -4,6 +4,10 @@ open System.Threading
 open FsUnitTyped
 open NUnit.Framework
 
+[<RequireQualifiedAccess>]
+module NonStaticTestHelpers =
+    let count = ref 0
+
 [<TestFixture>]
 type TestNonStatic () =
     let count = ref 0
@@ -12,8 +16,16 @@ type TestNonStatic () =
 
     [<Test>]
     member this.Foo () =
+        Interlocked.Increment NonStaticTestHelpers.count |> ignore<int>
         Interlocked.Increment count |> ignore<int>
-        this.Thing |> shouldEqual "I'm a thing"
+        this.Thing |> shouldEqual "i'm a thing"
+
+    [<Test>]
+    static member AnotherTest () =
+        Interlocked.Increment NonStaticTestHelpers.count |> ignore<int>
+        1 |> shouldEqual 1
 
     [<OneTimeTearDown>]
-    member _.TearDown () = count.Value |> shouldEqual 1
+    member _.TearDown () =
+        count.Value |> shouldEqual 1
+        NonStaticTestHelpers.count.Value |> shouldEqual 2
