8f890c491fa1c72268a0c816e8b019f428a8df57
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-23 11:55:19 +0100

    Also unnecessary

--- WoofWare.NUnitTestRunner/Program.fs
+++ WoofWare.NUnitTestRunner/Program.fs
@@ -10,12 +10,11 @@ open Spectre.Console
 // (This tells the DLL loader to look next to the test DLL for dependencies.)
 type SetBaseDir (testDll : FileInfo) =
     let oldBaseDir = AppContext.BaseDirectory
-    // do AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", testDll.Directory.FullName)
+    do AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", testDll.Directory.FullName)
 
     interface IDisposable with
         member _.Dispose () =
-            // AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", oldBaseDir)
-            ()
+            AppContext.SetData ("APP_CONTEXT_BASE_DIRECTORY", oldBaseDir)
 
 [<RequireQualifiedAccess>]
 type LogLevel =
